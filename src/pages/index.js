898964177914899f8a3e6e9202752282429176c0
Julian Goacher <julian.goacher@innerfunction.com>
2022-01-17 20:32:28 +0000

    Random bg colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, { useEffect } from 'react'
 
 import logoSVG from './situationist-logo.svg'
 
@@ -16,10 +16,17 @@ const logoStyles = {
     width: '344px'
 }
 
+const bgColors = ['goldenrod', 'yellow', 'green', 'blue', 'red']
+
 const Logo = () => (<img alt="situationi.st" src={logoSVG} style={logoStyles} />)
 
 // markup
 const IndexPage = () => {
+  useEffect(() => {
+    const color = bgColors[Math.floor(Math.random() * bgColors.length)]
+    document.documentElement.style.backgroundColor = color
+    document.body.style.backgroundColor = color
+  })
   return (
     <main style={pageStyles}>
         <Logo />
